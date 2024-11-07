14ef9fd33e30222a4a0b766c527c9e231930cf3b
naolleg <naollegesse51@gmail.com>
2024-11-07 23:00:19 +0300

    working on signin sign up pages

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,87 +1,71 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
-class Signin extends StatefulWidget {
+class Signin extends StatelessWidget {
   const Signin({super.key});
 
   @override
-  State<Signin> createState() => _SigninState();
-}
-
-class _SigninState extends State<Signin> {
-  @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> _signInKey = GlobalKey();
-    final TextEditingController _emailcontroller = TextEditingController();
-    final TextEditingController _passwordcontroller = TextEditingController();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
 
-    final RegExp _emailRegExp = RegExp(
-        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-    final RegExp _passwordRegExp = RegExp(
-        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
     return Scaffold(
+      backgroundColor: Colors.white, // White background
+      appBar: AppBar(
+        title: Text("Signin"),
+        backgroundColor: Color(0xFFFB8C00), // Dark Orange AppBar
+      ),
       body: Center(
-        child: Container(
-          margin: EdgeInsets.all(30),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 100, 0, 30),
-                child: Image.asset(
-                  "assets/image/logo.png",
-                  height: 100,
-                ),
-              ),
-              Text(
-                "LOGIN",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Color.fromARGB(255, 36, 174, 243),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Enter your email',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 14,
-                        horizontal: 20,
-                      )),
-                  controller: _emailcontroller,
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!_emailRegExp.hasMatch(value)) {
-                      return 'Invalid email address';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter your password',
-                    border: OutlineInputBorder(),
+              // Email TextField
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Enter your email',
+                  labelStyle:
+                      TextStyle(color: Color(0xFFFB8C00)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
                   ),
-                  controller: _passwordcontroller,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    if (!_passwordRegExp.hasMatch(value)) {
-                      return 'Password must be at least 8 characters, including uppercase, lowercase, number, and special character';
-                    }
-                    return null;
-                  },
                 ),
+              ),
+              SizedBox(height: 20),
+
+              // Password TextField
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Enter your password',
+                  labelStyle: TextStyle(
+                      color: Color.fromARGB(
+                          255, 175, 106, 20)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              // Sign In Button
+              ElevatedButton(
+                onPressed: () {
+                  print("Email: ${_emailController.text}");
+                  print("Password: ${_passwordController.text}");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(
+                      255, 33, 83, 189), // Dark Orange button color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text("Sign In", style: TextStyle(color: Colors.white)),
               ),
             ],
           ),
