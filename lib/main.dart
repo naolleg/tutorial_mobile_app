f4dc8b660d42e256b76e44e4dad92cfd72b65cde
naolleg <naollegesse51@gmail.com>
2024-11-08 22:35:24 +0300

    adding the signup form

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial_mobile_app/pages/homepage.dart';
 import 'package:tutorial_mobile_app/pages/signin.dart';
+import 'package:tutorial_mobile_app/pages/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Signin(),
+      home: Signup(),
     );
   }
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,15 +1,125 @@
+// ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures, no_leading_underscores_for_local_identifiers
+
 import 'package:flutter/material.dart';
 
-class Signup extends StatefulWidget {
+class Signup extends StatelessWidget {
   const Signup({super.key});
 
   @override
-  State<Signup> createState() => _SignupState();
-}
-
-class _SignupState extends State<Signup> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _lastnameController = TextEditingController();
+    final TextEditingController _firstnameController = TextEditingController();
+    final TextEditingController _cpasswordController = TextEditingController();
+    return Scaffold(
+      backgroundColor: Colors.white, // White background
+      appBar: AppBar(
+        title: Text("Signup"),
+        backgroundColor: Color(0xFFFB8C00), // Dark Orange AppBar
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Email TextField
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Enter your email',
+                  labelStyle:
+                      TextStyle(color: Color(0xFFFB8C00)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _firstnameController,
+                decoration: InputDecoration(
+                  labelText: 'Enter your first name',
+                  labelStyle:
+                      TextStyle(color: Color(0xFFFB8C00)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _lastnameController,
+                decoration: InputDecoration(
+                  labelText: 'Enter your last name',
+                  labelStyle:
+                      TextStyle(color: Color(0xFFFB8C00)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // Password TextField
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Enter your password',
+                  labelStyle: TextStyle(
+                      color: Color.fromARGB(
+                          255, 175, 106, 20)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _cpasswordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'conform your password',
+                  labelStyle: TextStyle(
+                      color: Color.fromARGB(
+                          255, 175, 106, 20)), // Dark Orange label
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(0xFFFB8C00)), // Dark Orange border
+                  ),
+                ),
+              ),
+
+              // Sign up Button
+              ElevatedButton(
+                onPressed: () {
+                  if (_passwordController.text == _cpasswordController.text) {
+                    print("Email: ${_emailController.text}");
+                    print("firstname: ${_firstnameController.text}");
+                    print("astname: ${_lastnameController.text}");
+                    print("Password: ${_passwordController.text}");
+                    print("cPassword: ${_cpasswordController.text}");
+                  } else
+                    print(" password didnot match");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(
+                      255, 33, 83, 189), // Dark Orange button color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text("Sign up", style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
